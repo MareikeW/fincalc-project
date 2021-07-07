8766035a7cf7adbe0dbe8dd6e786980cd05ce7a7
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-07-07 18:40:18 +0200

    Init react + react-router-dom + scss
    Installed react, react-router-dom, and SCSS. Set up Router in App.js.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            Home
+          </Route>
+
+          <Route exact path="/budgetplanner">
+            Budgetplanner
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
