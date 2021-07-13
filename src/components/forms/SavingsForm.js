67ee218f6d363324bb52f63339174c40c1f84090
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-07-13 10:37:43 +0200

    Bug: Added Yearly Endbalance for yearly compound
    Bug: The results in the Array with the yearly endbalances is not correct.

--- src/components/forms/SavingsForm.js
+++ src/components/forms/SavingsForm.js
@@ -26,8 +26,7 @@ const SavingsForm = () => {
 
     const calcEndSavingsBalance = (event) => {
         event.preventDefault();
-        let yearlyCompoundedBalance = [];
-        let yearlyCompoundedContribution = [];
+        let yearlyEndBalance = [];
 
         if (data.compound === "monthly") {
             /* Monthly Compound
@@ -36,23 +35,27 @@ const SavingsForm = () => {
             Total = [ 5000 (1 + 0.05 / 12) ^ (12 × 10) ] + [ 100 × (((1 + 0.00416)^(12 × 10) - 1) / (0.00416)) × (1 + 0.05 / 12) ]
             */
             for (let i = 1; i <= data.savingsPeriod; i++) {
-                yearlyCompoundedBalance.push(data.startingAmount * ((1 + ((data.returnRate / 100) / 12))**(12 * i)));
-                yearlyCompoundedContribution.push(data.contribution * (((1 + ((data.returnRate / 100) / 12))**(12 * i) - 1) / ((data.returnRate / 100) / 12)) * (1 + ((data.returnRate / 100) / 12)));
+                yearlyEndBalance.push((data.startingAmount * ((1 + ((data.returnRate / 100) / 12))**(12 * i))) + data.contribution * (((1 + ((data.returnRate / 100) / 12))**(12 * i) - 1) / ((data.returnRate / 100) / 12)) * (1 + ((data.returnRate / 100) / 12)));
             }
-       
-            setEndBalance(yearlyCompoundedBalance[yearlyCompoundedBalance.length -1] + yearlyCompoundedContribution[yearlyCompoundedContribution.length - 1]);
-            setTotalContributions(Number(data.startingAmount) + (Number(data.contribution * 12) * Number(data.savingsPeriod)));
-
             
+            console.log("Monthly Compound: " + yearlyEndBalance)    
         } else if (data.compound === "yearly") {
-            //console.log("Yearlyyy");
-            /* Yearly Compound
-            P = 5000. PMT = 100. r = 5/100 = 0.05 (decimal). n = 1. t = 10.
-            Total = [ P(1+r/n)^(nt) ] + [ PMT × (((1 + r/n)^(nt) - 1) / (r/n)) × (1+r/n) ]
-            Total = [ 5000 (1 + 0.05 / 1) ^ (1 × 10) ] + [ 100 × (((1 + 0.00416)^(1 × 10) - 1) / (0.00416)) * (1 + 0.05 / 1) ]
-            */
+            /* Yearly Compound 
+            Find the Bug:
+            Starting Amount: 1000
+            Monthly Contribution: 100
+            Return Rate: 10
+            Savings Period: 10 years
+            --> should be yearlyEndBalance = [2,364.05; 3,864.51; 5,515.02; 7,330.57; 9,327.68; 11,524.51; 13,941.01; 16,599.17; 19,523.14; 22,739.50 ]*/
+            for (let i = 1; i <= data.savingsPeriod; i++) {
+                yearlyEndBalance.push((data.startingAmount * ((1 + ((data.returnRate / 100) / 1))**(1 * i))) + data.contribution * (((1 + ((data.returnRate / 100) / 1))**(1 * i) - 1) / ((data.returnRate / 100) / 1)) * (1 + ((data.returnRate / 100) / 1)));
+            }
             
+            console.log("Yearly Compound: " + yearlyEndBalance)
         }
+
+        setEndBalance(yearlyEndBalance[yearlyEndBalance.length - 1]);
+        setTotalContributions(Number(data.startingAmount) + (Number(data.contribution * 12) * Number(data.savingsPeriod)));
     }
 
     useEffect(() => {
