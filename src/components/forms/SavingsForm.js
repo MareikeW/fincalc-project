842c29150d39492c578e3c2f7db5fdae2ff915e9
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-07-14 15:54:30 +0200

    Yearly and monthly compound calculation now works
    Calculation is now with deposits at the end of the month instead of the beginning of the month.

--- src/components/forms/SavingsForm.js
+++ src/components/forms/SavingsForm.js
@@ -27,28 +27,22 @@ const SavingsForm = () => {
     const calcEndSavingsBalance = (event) => {
         event.preventDefault();
         let yearlyEndBalance = [];
+        
 
         if (data.compound === "monthly") {
             /* Monthly Compound
             P = 5000. PMT = 100. r = 5/100 = 0.05 (decimal). n = 12. t = 10.
-            Total = [ P(1+r/n)^(nt) ] + [ PMT × (((1 + r/n)^(nt) - 1) / (r/n)) × (1+r/n) ]
-            Total = [ 5000 (1 + 0.05 / 12) ^ (12 × 10) ] + [ 100 × (((1 + 0.00416)^(12 × 10) - 1) / (0.00416)) × (1 + 0.05 / 12) ]
+            Total = [ P(1+r/n)^(nt) ] + [ PMT × (((1 + r/n)^(nt) - 1) / (r/n)) ]
             */
             for (let i = 1; i <= data.savingsPeriod; i++) {
-                yearlyEndBalance.push((data.startingAmount * ((1 + ((data.returnRate / 100) / 12))**(12 * i))) + data.contribution * (((1 + ((data.returnRate / 100) / 12))**(12 * i) - 1) / ((data.returnRate / 100) / 12)) * (1 + ((data.returnRate / 100) / 12)));
+                yearlyEndBalance.push((data.startingAmount * ((1 + ((data.returnRate / 100) / 12))**(12 * i))) + data.contribution * (((1 + ((data.returnRate / 100) / 12))**(12 * i) - 1) / ((data.returnRate / 100) / 12)));
             }
-            
+
             console.log("Monthly Compound: " + yearlyEndBalance)    
         } else if (data.compound === "yearly") {
-            /* Yearly Compound 
-            Find the Bug:
-            Starting Amount: 1000
-            Monthly Contribution: 100
-            Return Rate: 10
-            Savings Period: 10 years
-            --> should be yearlyEndBalance = [2,364.05; 3,864.51; 5,515.02; 7,330.57; 9,327.68; 11,524.51; 13,941.01; 16,599.17; 19,523.14; 22,739.50 ]*/
+            /* Yearly Compound */
             for (let i = 1; i <= data.savingsPeriod; i++) {
-                yearlyEndBalance.push((data.startingAmount * ((1 + ((data.returnRate / 100) / 1))**(1 * i))) + data.contribution * (((1 + ((data.returnRate / 100) / 1))**(1 * i) - 1) / ((data.returnRate / 100) / 1)) * (1 + ((data.returnRate / 100) / 1)));
+                yearlyEndBalance.push((data.startingAmount * ((1 + (data.returnRate / 100))**i)) + data.contribution * ((((1 + (data.returnRate / 100))**i) - 1) / ((data.returnRate / 100) / 12)));
             }
             
             console.log("Yearly Compound: " + yearlyEndBalance)
