aaf61b18362577e94ceaec2f8c1bac270e45b7ba
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-07-26 10:18:41 +0200

    Added chart to budget page

---
+++ src/components/charts/BudgetChart.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Doughnut } from 'react-chartjs-2';
+import "./charts-styles.scss";
+
+const BudgetChart = ({budgetData}) => {
+    const {saving, housing, utilities, food, transportation, clothing, misc, personal, recreation, charity, debt} = budgetData;
+
+    const data = {
+        labels: ['Savings', 'Housing', 'Utilities', 'Food', 'Transportation', 'Clothing', 'Miscellaneous', 
+                'Personal', 'Recreation', 'Charity', 'Debt'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [Number(saving), Number(housing), Number(utilities), Number(food), Number(transportation), 
+                Number(clothing), Number(misc), Number(personal), Number(recreation), Number(charity), Number(debt)],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+    };
+
+
+    return (
+        <div className="budget__chart">
+            <Doughnut 
+                data={data}
+            /> 
+        </div>
+    )
+}
+
+export default BudgetChart;
