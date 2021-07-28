322fa1622638e444bf4bba4f3af6ca02519bb480
Nithish Kumar <nithishpadma@gmail.com>
2021-07-28 18:56:13 +0530

    refactor OnetimeInvestmentChart.jsx

--- src/components/charts/OneTimeInvChart.jsx
+++ src/components/charts/OneTimeInvChart.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Line } from 'react-chartjs-2'
-import './charts-styles.scss'
+import React from "react"
+import { Line } from "react-chartjs-2"
+import "./charts-styles.scss"
 
 const options = {
   scales: {
@@ -15,24 +15,25 @@ const options = {
 };
 
 const OneTimeInvChart = ({ endBalance }) => {
-  console.log(endBalance)
+
   const data = {
-    labels: Array.from({ length : endBalance.length}, (_,idx) => idx+1),
-    datasets : [
+    labels: Array.from({ length: endBalance.length }, (_, idx) => idx + 1),
+    datasets: [
       {
-        label: '# of EndBalance',
+        label: "# of EndBalance",
         data: endBalance,
         fill: false,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgba(255, 99, 132, 0.2)',
-      }
-    ]
-  }
-  return(
-    <div className='compound__chart'>
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
+      },
+    ],
+  };
+
+  return (
+    <div className="compound__chart">
       <Line data={data} options={options} />
     </div>
-  )
-}
+  );
+};
 
 export default OneTimeInvChart
