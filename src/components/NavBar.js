cfca701fae74cbd5e8661e47adc37ce510494d18
Alana Caporale <38472545+alanacapcreates@users.noreply.github.com>
2021-08-09 12:27:11 -0400

    Added 'Home' link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ const NavBar = () => {
     return (
         <nav>
             <ul>
+                <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/budgetplanner'}>Budget Planner</Link></li>
                 <li><Link to={'/savings-calculator'}>Savings Calculator</Link></li>
                 <li><Link to={'/onetime-investment'}>One-time Investment</Link></li>
