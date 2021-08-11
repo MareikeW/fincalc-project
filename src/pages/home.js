05a751d4d738c0feeda7417d6feaa1e65936e320
Alana Caporale <38472545+alanacapcreates@users.noreply.github.com>
2021-08-11 15:57:12 -0400

    edits to responsiveness

--- src/pages/home.js
+++ src/pages/home.js
@@ -15,8 +15,7 @@ const Home = () => {
             </div>
             <div class = "card-container">
                 <div class = "card">
-                        <img src="https://images.unsplash.com/photo-1564939558297-fc396f18e5c7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80" 
-                        alt = "calculator"/>
+                        <img src="https://images.unsplash.com/photo-1564939558297-fc396f18e5c7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80" alt = "calculator"/>  
                     <div class = "card-text">
                         <h2>Budget Planner!</h2>
                         <p>Click here to plan your budget!</p>
